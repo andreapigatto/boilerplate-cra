11b4c3f2b34177700fe69efa69c0526eb5f0a703
andreapigatto <andreapigatto01@gmail.com>
2021-07-09 12:22:03 +0200

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="text-3xl text-center text-blue-500 mt-16">
+        Create React App Boilerplate
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
